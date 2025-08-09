28e758eae1e556d82d9fdafabd6a0c3d14da3736
LPATROCINIO69 <luciopp69@gmail.com>
2025-08-09 11:23:23 -0300

    Inclusão de estilo Input.css no frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/Button";
+
 function App() {
   return (
     <div>
       <h1>Bem-vindo ao Sistema de Oportunidades</h1>
+      <Button>Teste</Button>
     </div>
   );
 }
